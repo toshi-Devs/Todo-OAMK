708d1e2498df0880b77fda974afd8b575512d19a
toshi-Devs <trahim87mustapha@gmail.com>
2024-03-17 23:22:14 +0200

    part 4

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
+
 const BACKEND_ROOT_URL = 'http://localhost:3001';
+
+import { Todos } from './class/Todos.js';
+
+const todos = new Todos(BACKEND_ROOT_URL);
 
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
@@ -9,22 +14,35 @@ input.disabled = true;
 const renderTask = (task) => {
     const li = document.createElement('li');
     li.setAttribute('class', 'list-group-item');
-    li.innerHTML = task;
+    li.innerHTML = task.getText()
     list.appendChild(li);
 }
 
-const getTasks = async () => {
-    try {
-        const response = await fetch(BACKEND_ROOT_URL);
-        const json = await response.json();
-        json.forEach(task => {
-            renderTask(task.description);
-        });
+const getTasks = () => {
+    // try {
+    //     const response = await fetch(BACKEND_ROOT_URL);
+    //     const json = await response.json();
+    //     json.forEach(task => {
+    //         renderTask(task.description);
+    //     });
+    //     input.disabled = false;
+    // } catch (error) {
+    //     console.error('Error:', error);
+    //     alert('Error fetching tasks' + error);
+    // }
+
+    todos.getTasks().then((tasks) => {
+        tasks.forEach(task => {
+            renderTask(task);
+            
+        }
+        )
         input.disabled = false;
-    } catch (error) {
+    }).catch((error) => {
         console.error('Error:', error);
         alert('Error fetching tasks' + error);
-    }
+    })
+        // input.disabled = false;
 }
 
 const saveTask = async (task) => {
@@ -51,11 +69,12 @@ input.addEventListener('keypress', (e) => {
     if(e.key === 'Enter') {
         e.preventDefault();
         const task = input.value.trim();
-        if (task !=='') {
-            saveTask(task).then((json) => {
+        if (task !== '') {
+            todos.addTask(task).then((task) => {
                 renderTask(task);
                 input.value = '';
-            });
+                input.focus();
+            })
         } 
     }
 });

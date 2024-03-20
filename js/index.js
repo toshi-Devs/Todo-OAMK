d02a91dc3f9bd1ec9f47270b3d4df905e344ccd5
toshi-Devs <trahim87mustapha@gmail.com>
2024-03-20 15:49:32 +0200

    Part 8 frontEnd - button working

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ const todos = new Todos(BACKEND_ROOT_URL);
 
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
+const button = document.querySelector('button');
 
 input.disabled = true;
 
@@ -45,18 +46,6 @@ const renderLink = (li, id) => {
 }
 
 const getTasks = () => {
-    // try {
-    //     const response = await fetch(BACKEND_ROOT_URL);
-    //     const json = await response.json();
-    //     json.forEach(task => {
-    //         renderTask(task.description);
-    //     });
-    //     input.disabled = false;
-    // } catch (error) {
-    //     console.error('Error:', error);
-    //     alert('Error fetching tasks' + error);
-    // }
-
     todos.getTasks().then((tasks) => {
         tasks.forEach(task => {
             renderTask(task);
@@ -90,6 +79,17 @@ const saveTask = async (task) => {
 
 getTasks();
 
+button.addEventListener('click', () => {
+    const task = input.value.trim();
+    if (task !== '') {
+        todos.addTask(task).then((task) => {
+            renderTask(task);
+            input.value = '';
+            input.focus();
+        })
+    }
+}
+);
 
 input.addEventListener('keypress', (e) => {
     if(e.key === 'Enter') {

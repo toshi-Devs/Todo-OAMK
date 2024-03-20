d64148b051678d1b5c5a646622b28668c06e49bf
toshi-Devs <trahim87mustapha@gmail.com>
2024-03-20 14:42:37 +0200

    env.vault added

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -23,6 +23,7 @@ const openDb = () => {
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
+        ssl: process.env.SSL
 
     });
     return pool;

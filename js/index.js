99ce3104e4e3a1dccd8a4ae72a877c2f3311a506
toshi-Devs <trahim87mustapha@gmail.com>
2024-03-20 15:38:18 +0200

    Part 8 frontEnd

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
-const BACKEND_ROOT_URL = 'http://localhost:3001';
+// const BACKEND_ROOT_URL = 'http://localhost:3001';
+const BACKEND_ROOT_URL = 'https://todo-backend-hyby.onrender.com';
 
 import { Todos } from './class/Todos.js';
 
